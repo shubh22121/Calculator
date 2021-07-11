8e5ba010f3aa8f83168999dc416bf95e4f7996d5
shubham <shubham22121@gmail.com>
2021-07-12 02:14:08 +0530

    removed console statement

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let screenvalue = "";
 for (item of buttons) {
   item.addEventListener("click", (e) => {
     buttonText = e.target.innerText;
-    console.log("button test is", buttonText);
+    
     if (buttonText == "x") {
       buttonText = "*";
       screenvalue += buttonText;
